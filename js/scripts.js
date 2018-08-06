f8fca221d898389d9989aeaa7932af0149fdf36d
Chris Gosser and hingyi khong <hingyi@>
2018-08-06 16:49:50 -0700

    create kitchen converter app, 3 functions.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function liquid (gallons) {
+  return gallons/2.6417e-1};
+// var gallons = prompt("input number of gallons you need to convert");
+// alert(liquid(gallons));
+function ozml (ounces) {
+  return ounces/ 3.33814e-2
+}
+// var ounces = prompt("input number of ounces you need to convert");
+// alert(ozml(ounces));
+
+function pintsLiters (pints) {
+  return pints/ 1.7598
+};
+var pints = prompt("input number of pints you need to convert");
+alert(pintsLiters(pints));
